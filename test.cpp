bede45f357fcaf74c9526797b8204cd097cac7de
Yago-Mizuma-codes <108910009+Yago-Mizuma-codes@users.noreply.github.com>
2022-07-07 22:39:40 -0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main (){
+	char nome [10];
+	char finish[10];
+	std::cout <<"oi qual e o seu nome?";
+	std::cin >> nome;
+	std::cout << "ola "<<nome<<" bem vindo ao futuro";
+	std::cin>>finish;
+	return 0;
+}
